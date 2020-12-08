6577f6ae2b68ff4c457bf58446e2513cdf6c86ca
Belinda Baumgartner <61059375+bbaumgartner74@users.noreply.github.com>
2020-12-08 19:33:31 +0100

    Update e5e_test.js

--- e5e_test.js
+++ e5e_test.js
@@ -1,12 +1,12 @@
-const user = process.env.USER;
-//console.log(`Your name is ${user}`);
+// const user = process.env.USER;
+// console.log(`Your name is ${user}`);
 
 exports.myfunction = (event, context) => {
   console.log(event);
   console.log(context);
   
   let cities = event.params.cities;
-  
+  let user = event.params.user[0]
 
   return {
     data: `Hello ${user}, you live in ${cities[0]}!`,
