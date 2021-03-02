c88f9274398749e3cb8df9e2ee35d773cedf395f
Belinda Baumgartner <61059375+bbaumgartner74@users.noreply.github.com>
2021-03-02 12:18:23 +0100

    Update e5e-docs.js

--- e5e-docs.js
+++ e5e-docs.js
@@ -2,6 +2,8 @@ const user = process.env.USER;
 console.log(user);
 
 exports.myfunction = (event, context) => {
+  console.log(`event: ${event}`);
+  console.log(`context: ${context}`);
   
   return {
     data: `Hello ${user}!`,
